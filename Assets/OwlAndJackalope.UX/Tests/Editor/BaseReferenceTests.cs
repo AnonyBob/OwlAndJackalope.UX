e14f074c719f11e0fd741a66cca104aaf33f0218
AnonyBob <nicholas.stri@gmail.com>
2021-09-26 22:13:22 -0500

    Added some test stubs for observers and references

--- Assets/OwlAndJackalope.UX/Tests/Editor/BaseReferenceTests.cs
+++ Assets/OwlAndJackalope.UX/Tests/Editor/BaseReferenceTests.cs
@@ -5,6 +5,104 @@ namespace OwlAndJackalope.UX.Tests.Editor
     [TestFixture]
     public class BaseReferenceTests
     {
+        public void InitializeWithDetailsHasDetails()
+        {
+            
+        }
+
+        public void InitializeWithNoDetailsHasNoDetails()
+        {
+            
+        }
+
+        public void InitializeHasZeroVersion()
+        {
+            
+        }
+
+        public void AddNewDetailChangesVersion()
+        {
+            
+        }
+
+        public void AddExistingDetailWithOverrideChangesVersion()
+        {
+            
+        }
+
+        public void AddExisitngDetailWithNoOverrideDoesNothing()
+        {
+            
+        }
         
+        public void AddNewDetailsChangesVersion()
+        {
+            
+        }
+
+        public void AddExistingDetailsWithOverrideChangesVersion()
+        {
+            
+        }
+
+        public void AddExisitngDetailsWithNoOverrideDoesNothing()
+        {
+            
+        }
+
+        public void RemoveDetailRemovesDetail()
+        {
+            
+        }
+
+        public void RemoveUnaddedDetailDoesNothing()
+        {
+            
+        }
+
+        public void GetDetailRetrievesDetail()
+        {
+            
+        }
+
+        public void GetDetailThatDoesntExistReturnsNull()
+        {
+            
+        }
+
+        public void GetDetailOfTypeRetrievesDetail()
+        {
+            
+        }
+
+        public void GetDetailOfTypeThatDoesntExistReturnsNull()
+        {
+            
+        }
+
+        public void GetDetailOfTypeThatDoesExistWithDifferentTypeReturnsNull()
+        {
+            
+        }
+
+        public void EnumerationGoesThroughAllDetails()
+        {
+            
+        }
+
+        public void ConvertCreatesDetails()
+        {
+            
+        }
+
+        public void UpdateSerializedDetailsAddsNewDetails()
+        {
+            
+        }
+
+        public void UpdateSerializedDetailsDoesntRemoveExistingDetails()
+        {
+            
+        }
     }
 }
