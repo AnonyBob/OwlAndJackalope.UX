efea3d2caf807049115b430b44a1c0b057d17b58
AnonyBob <nicholas.stri@gmail.com>
2022-01-17 16:08:17 -0600

    Added a new multi state binder for the game object setup.

---
+++ Assets/OwlAndJackalope.UX/Runtime/StateBinders/MultiGameObjectStateBinder.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace OwlAndJackalope.UX.Runtime.StateBinders
+{
+    public class MultiGameObjectStateBinder : MultiStateBinder<MultiGameObjectStateBinder.GameObjectStateHandler>
+    {
+        [System.Serializable]
+        public class GameObjectStateHandler : StateActionHandler
+        {
+            [SerializeField]
+            private GameObject[] _targets;
+            
+            protected override void PerformChange(bool isActive)
+            {
+                foreach (var target in _targets)
+                {
+                    if (target != null)
+                    {
+                        target.SetActive(isActive);
+                    }
+                }
+            }
+        }
+    }
+}
