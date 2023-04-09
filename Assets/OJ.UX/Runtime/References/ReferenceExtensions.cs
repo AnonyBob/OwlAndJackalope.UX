33e806f223d4a97d0b66f8a73765f5c619ab8fd2
AnonyBob <nicholas.stri@gmail.com>
2023-04-08 23:41:32 -0500

    Updated the abstract serialized detail to remove the addressable requirement. Added new extension methods to make getting things as mutable easier.

---
+++ Assets/OJ.UX/Runtime/References/ReferenceExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace OJ.UX.Runtime.References
+{
+    public static class ReferenceExtensions
+    {
+        public static IMutableDetail<T> GetMutableDetail<T>(this IReference reference, string detailName)
+        {
+            return reference.GetDetail<T>(detailName) as IMutableDetail<T>;
+        }
+
+        public static ListDetail<T> GetListDetail<T>(this IReference reference, string detailName)
+        {
+            return reference.GetDetail<List<T>>(detailName) as ListDetail<T>;
+        }
+    }
+}
--- Assets/OJ.UX/Runtime/References/Serialized/AddressableSerializedDetail.cs
+++ Assets/OJ.UX/Runtime/References/Serialized/AddressableSerializedDetail.cs
@@ -1,14 +1,13 @@
-﻿using System;
+﻿#if ADDRESSABLES_ACTIVE
+using System;
 using UnityEngine.AddressableAssets;
 
 namespace OJ.UX.Runtime.References.Serialized
 {
-#if ADDRESSABLES_ACTIVE
-    
     [Serializable, SerializedDetailDisplay("AssetReference")]
     public class AddressableSerializedDetail : SerializedValueDetail<AssetReference>
     {
     }
+}
     
 #endif
-}
